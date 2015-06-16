d5ad0ab8febb505289b56d3b49b7b436e077ed6f
chacedev <andy0711@gmail.com>
2015-06-16 17:12:25 +1200

    laptop workstation setup

---
+++ public/user_page.php
@@ -0,0 +1,15 @@
+<?php
+
+# user_page.php
+
+
+# Load libraries and models
+
+
+# Logic
+
+
+# Load views
+include '../views/header.php';
+include '../views/images.php';
+include '../views/footer.php';
