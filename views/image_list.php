7e69a36ef3b98ba195cf9a9de7bfc28d2757940c
Riaan Lambrechts <riaan+github@vendhq.com>
2015-06-16 16:33:52 +1200

    fix up list

--- views/image_list.php
+++ views/image_list.php
@@ -1,27 +1,24 @@
-<<<<<<< HEAD
 <h1>image list</h1>
-=======
-	<main>
-		<div class="row">
-			<ul>
-				<li><img src="" alt=""></li>
-				<li><img src="" alt=""></li>
-				<li><img src="" alt=""></li>
-			</ul>
-		</div>
-		<div class="row">
-			<ul>
-				<li><img src="" alt=""></li>
-				<li><img src="" alt=""></li>
-				<li><img src="" alt=""></li>
-			</ul>
-		</div>
-		<div class="row">
-			<ul>
-				<li><img src="" alt=""></li>
-				<li><img src="" alt=""></li>
-				<li><img src="" alt=""></li>
-			</ul>
-		</div>
-	</main>
->>>>>>> origin/master
+<main>
+	<div class="row">
+		<ul>
+			<li><img src="" alt=""></li>
+			<li><img src="" alt=""></li>
+			<li><img src="" alt=""></li>
+		</ul>
+	</div>
+	<div class="row">
+		<ul>
+			<li><img src="" alt=""></li>
+			<li><img src="" alt=""></li>
+			<li><img src="" alt=""></li>
+		</ul>
+	</div>
+	<div class="row">
+		<ul>
+			<li><img src="" alt=""></li>
+			<li><img src="" alt=""></li>
+			<li><img src="" alt=""></li>
+		</ul>
+	</div>
+</main>
