dff12fba150e2307101d848f7a8ac6fc5cb8e6d6
Hwan Joo Kim <you@example.co>
2015-06-17 11:23:17 +1200

    foreach loop in image_list.php

--- views/image_list.php
+++ views/image_list.php
@@ -1,24 +1,10 @@
 <h1>image list</h1>
 <main>
-	<div class="row">
-		<ul>
-			<li><img src="" alt=""></li>
-			<li><img src="" alt=""></li>
-			<li><img src="" alt=""></li>
-		</ul>
-	</div>
-	<div class="row">
-		<ul>
-			<li><img src="" alt=""></li>
-			<li><img src="" alt=""></li>
-			<li><img src="" alt=""></li>
-		</ul>
-	</div>
-	<div class="row">
-		<ul>
-			<li><img src="" alt=""></li>
-			<li><img src="" alt=""></li>
-			<li><img src="" alt=""></li>
-		</ul>
-	</div>
+	<ul>
+	<?php foreach ($images as $image): ?>
+
+		<li><img src="<?php echo $image->url; ?>" alt="<?php echo $image->caption; ?>"></li>
+
+	<?php endforeach ; ?>
+	</ul>
 </main>
