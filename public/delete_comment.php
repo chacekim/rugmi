17f1123e5bec0d2e05c74dd507d332c28b2a52b6
Riaan Lambrechts <riaan+github@vendhq.com>
2015-06-18 18:35:02 +1200

    added delete for comments

---
+++ public/delete_comment.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// 1 load libraries & models
+// 
+require_once '../libraries/auth.lib.php';
+require_once '../libraries/url.lib.php';
+require_once '../models/comment.model.php';
+
+// 2 logic
+
+$comment = new Comment();
+
+$comment->load($_GET['id']);
+
+$comment->delete();
+
+
+
+
+// 3 views
+
+// URL::redirect('view_image.php?id='.$comment->image_id);
+URL::redirect('view_image.php?id='.$_GET['image_id']);
