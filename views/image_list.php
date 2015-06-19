19dc21573150cf859481fd2478f8bb317e0290ad
Harry Cardno <harry.cardno@43492-205.auckland.natcoll.ac.nz>
2015-06-19 15:30:29 +1200

    did some changes

--- views/image_list.php
+++ views/image_list.php
@@ -6,11 +6,13 @@
 	
 	<?php foreach ($images->items as $image): ?>
 
-
+<<<<<<< HEAD
+		<img src="<?php echo $image->url ?>" alt="<?php echo $image->caption ?>" class="flex-item">
+=======
 		<li>
-			<a href=<?php echo $image->id ?>"><img src="assets/img/<?php echo $image->url ?>?id=<?php echo $image->id ?>" alt="<?php echo $image->caption ?>" class="flex-item"></a>
+			<a href="view_image.php?id=<?php echo $image->id ?>"><img src="assets/img/<?php echo $image->url ?>?id=<?php echo $image->id ?>" alt="<?php echo $image->caption ?>" class="flex-item"></a>
 		</li>
-
+>>>>>>> a22887bcbf0b1d2f000908220ce7e2f75c101c44
 
 	<?php endforeach ; ?>
 	</ul>
